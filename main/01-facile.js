a7008bca8389901b8de0f9c7dde7e7efbd590cb3
Camille38490 <camillehurel28@gmail.com>
2022-02-12 17:42:31 +0000

    <contrôle>

--- main/01-facile.js
+++ main/01-facile.js
@@ -5,13 +5,38 @@
  * - "NUL" s'il est nul
  */
 function signe(nombre) {
+    if (nombre > 0) {
+        return "positif"
+    } else if (nombre < 0) {
+    } else {
+        return "nul"
+    }
+    console.log(signe(2))
+    /**
+     * Programmer une fonction prenant en argument un tableau de nombres, et qui renvoie un tableau ne contenant que les nombres pairs.
+     * Astuce : un nombre pair est un nombre dont le reste de la division par 2 est 0.
+     */
+    function nombresPairs(nombres) {
+        let nombres = [];
+        let result = [];
+        for (let i = 1; i <= 25; i++) {
+            nombre.push([i])
+        }
+        for (let i = 0; i < nombres.length; i++) {
+            if (nombresPairs(nombres[i]) == true) {
+                result.push(nombre[i])
+            }
+        }
+console.log()
+console.log("Exo 2")
+console.log("Tableau Input:" +nombres)
+console.log("Tableau nombres pairs:;"+result)
 
-}
-
-/**
- * Programmer une fonction prenant en argument un tableau de nombres, et qui renvoie un tableau ne contenant que les nombres pairs.
- * Astuce : un nombre pair est un nombre dont le reste de la division par 2 est 0.
- */
-function nombresPairs(nombres) {
-
+function nombresPairs(x){
+    if (x%2===0){
+        return true;
+    }
+    else{
+        return false;
+    }
 }
--- main/02-intermediaire.js
+++ main/02-intermediaire.js
@@ -4,30 +4,64 @@
  * Exemple : [1, 2, 3] et ["apple", "orange", "banana"] renverra : [1, "apple", 2, "orange", 3, "banana"]
  */
 function combine(tableau1, tableau2) {
+    t: String[3] = ["1", "2", "3"];
+    t2: String[3] = ["apple", "orange", "banana"];
 
-}
+    tCombinaison: String[6];
+    for (i: Integer = 0; i++) {
+        tCombinaison[i * 2] = t[i];
+        tCombinaison[i * 2 + 1] = t2[i];
+    }
 
-/**
- * Programmer une fonction prenant en argument un tableau d'éléments et une valeur offset, et qui renvoie un tableau avec les valeurs du tableau en argument décalées de la valeur
- * de offset.
- * 
- * Par exemple : rotate([1, 2, 3, 4, 5], 2) renverra [4, 5, 1, 2, 3] 
- */
-function rotate(tableau, offset) {
+    /**
+     * Programmer une fonction prenant en argument un tableau d'éléments et une valeur offset, et qui renvoie un tableau avec les valeurs du tableau en argument décalées de la valeur
+     * de offset.
+     * 
+     * Par exemple : rotate([1, 2, 3, 4, 5], 2) renverra [4, 5, 1, 2, 3] 
+     */
+    function rotate(tableau, offset) {
+        let tableau_elements = [];
+        for (let i = 1; i <= 5; i++) {
+            tableau_elements =.push([i])
+        }
+    }
+    function rotate(tableu, offset) {
+        let tableau_result = [];
+        let n = tableau.length;
+        let limite = n - offset;
+        let i = 0;
+        if (offset > n) {
+            return ("offset invalide");
+        }
+        else {
+            for (i = limite; i < n; i++) {
+                tableau_result.at(tableu[i]);
+            }
+            return tableau_result;
+        }
+    }
 
-}
 
-/**
- * Suite de Syracuse
- * 
- * La suite de Syracuse est une suite de nombre (nous avions vu la suite de Fibonacci en cours), calculée en prenant pour chaque élément la moitié de sa valeur s'il est pair
- * et le triple plus 1 s'il est impair. Cette suite est infinie, et nous ne prendrons que les 10 premiers éléments.
- * 
- * Programmer une fonction prenant en argument le nombre pour lequel la suite est calculée et qui renvoie un tableau contenant les 10 premiers éléments de la suite de Syracuse.
- * 
- * Par exemple, pour 14, la suite sera : [14, 7, 22, 11, 34, 17, 52, 26, 13, 40]
- * Pour 1, la suite sera : [1, 4, 2, 1, 4, 2, 1, 4, 2, 1]
- */
-function syracuse(nombre) {
-
-}
+    /**
+     * Suite de Syracuse
+     * 
+     * La suite de Syracuse est une suite de nombre (nous avions vu la suite de Fibonacci en cours), calculée en prenant pour chaque élément la moitié de sa valeur s'il est pair
+     * et le triple plus 1 s'il est impair. Cette suite est infinie, et nous ne prendrons que les 10 premiers éléments.
+     * 
+     * Programmer une fonction prenant en argument le nombre pour lequel la suite est calculée et qui renvoie un tableau contenant les 10 premiers éléments de la suite de Syracuse.
+     * 
+     * Par exemple, pour 14, la suite sera : [14, 7, 22, 11, 34, 17, 52, 26, 13, 40]
+     * Pour 1, la suite sera : [1, 4, 2, 1, 4, 2, 1, 4, 2, 1]
+     */
+    function syracuse(nombre) {
+        let ma_syracuse = 3;
+        function syracuse(nombre) {
+            let array = [nombre];
+            for (let i = 0; i < 9; i++)
+                if ((array[i] % 2 === 0) == true) {
+                }
+                else {
+                    array.push((array[i] * 3) + 1)
+                }
+            return array
+        }
